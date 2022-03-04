df2da75b790a46de3fae00bbed71b620a2de43af
Charbel Hanna <charbel_hanna@hotmail.com>
2022-03-04 16:12:47 +0100

    recent changes to gitignore

--- Scripts/Aks-Create.ps1
+++
@@ -1,4 +0,0 @@
-az group create -name komo-aks-test-rg --location westeurope
-az group create --name komoaksclt01-Resources-rg --location westeurope
-az aks create --resource-group komo-aks-test-rg --name komoaksclt01 --node-count 1 --generate-ssh-keys --node-resouce-group
-az aks get-credentials --resource-group myResourceGroup --name myAKSCluster
